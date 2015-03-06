5b53b32873154b2faef78d8e799ca5340367d139
Walter Dolce <walterdolce@gmail.com>
2015-03-06 23:22:48 +0000

    Define initial setup.py parameters

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
-__author__ = 'walterdolce'
+from distutils.core import setup
+
+setup(
+    name="modsync",
+    description="Files & directories watcher & synchronizer",
+    version="0.1.0",
+    url="https://walterdolce@bitbucket.org/walterdolce/modsync",
+    author="Walter Dolce",
+    author_email="walterdolce@gmail.com",
+    packages=["modsync"],
+    classifiers=[
+        "Programming Language :: Python",
+        # TODO License classifier
+        # TODO Python versions compatibility classifier
+        # TODO OSes compatibility classifier
+    ]
+)
